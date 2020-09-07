eb255889a4374e7416db61b981bedbf18b9b49a6
alessonsousa <alessonsousaviana@gmail.com>
2020-09-07 14:21:32 -0300

    v1.0

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+
+Uma faculdade precisa organizar os seus dados acadêmicos. O diretor informou as seguintes necessidades iniciais para a construção de um software:
+Cadastro de Professores: nome, idade, cpf, telefone, e-mail, formação e endereço;
+Cadastro de Aluno: nome, idade, cpf, telefone, email e endereço;
+Cadastro de Curso: código e nome;
+Cadastro de Disciplina: código, nome, curso, carga horária, professor e alunos;
+Cadastro de endereço: logradouro, número, bairro, cidade e estado.
+Construa registros para atender as essas necessidades iniciais do diretor, observando os dados de cada item acima e as relações entre eles.
+
+*/
+using namespace std;
+
+
+
+int main() {
+	
+
+	return 0;
+}
